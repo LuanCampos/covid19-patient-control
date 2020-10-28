1f1472aca52f426389b6ab337ad68c9db925bafc
Luan Campos <campos.luan@outlook.com>
2020-10-28 11:55:53 -0300

    Add pages
    Add pages and functions.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <unistd.h>
 #include <windows.h>
 
 typedef struct data DATA;
@@ -26,8 +27,8 @@ typedef struct paciente PACIENTE;
 struct paciente
 {
     char nome[30];
-    int cpf;
-    int telefone;
+    char cpf[12];
+    char telefone[15];
     LOCAL endereco;
     DATA nasc;
     char email[30];
@@ -80,7 +81,25 @@ int main()
         scanf("%s", &login);
 
         SetCursorPosition(28, 10);
-        scanf("%s", &senha);
+        int p=0;
+        do
+        {
+            senha[p]=getch();
+            if(senha[p]!='\r' && senha[p]!='\b')
+            {
+                printf("*");
+            }
+            if(senha[p]=='\b' && p >= 1)
+            {
+                p-=2;
+                SetCursorPosition(29+p, 10);
+                printf(" ");
+                SetCursorPosition(29+p, 10);
+            }
+            p++;
+        }
+        while(senha[p-1]!='\r');
+        senha[p-1]='\0';
 
         SetCursorPosition(0, 20);
 
@@ -126,24 +145,140 @@ int main()
         switch(opcao)
         {
             case 1:
-                //Instruções
-            break;
+            {
+                FILE *listaPacientes;
+                listaPacientes = fopen("listaPacientes.txt", "r");
+
+                if (listaPacientes == NULL)
+                {
+                    printf("  [!] Não há pacientes cadastrados! Pressione ENTER para continuar...");
+                    break;
+                }
+
+                char pacientes[1500];
+
+                while (fgets(pacientes, 1500, listaPacientes) != NULL)
+                {
+                    printf("%s", pacientes);
+                }
+
+                fclose(listaPacientes);
+
+                break;
+            }
 
             case 2:
-                //Instruções
-            break;
+            {
+                FILE *grupoDeRisco;
+                grupoDeRisco = fopen("grupoDeRisco.txt", "r");
+
+                if (grupoDeRisco == NULL)
+                {
+                    printf("  [!] Não há pacientes com comorbidades! Pressione ENTER para continuar...");
+                    break;
+                }
+
+                char comorbidades[1500];
+
+                while (fgets(comorbidades, 1500, grupoDeRisco) != NULL)
+                {
+                    printf("%s", comorbidades);
+                }
+
+                fclose(grupoDeRisco);
+
+                break;
+            }
 
             case 3:
-                //Instruções
-            break;
+            {
+                PACIENTE novoPaciente;
+
+                system("cls");
+                printf("\n  #-------------------  CADASTRO DE PACIENTES (COVID 19)  -------------------#");
+                printf("\n  |                                                                          |");
+                printf("\n  |                       CADASTRO DE NOVO PACIENTE                          |");
+                printf("\n  |                                                                          |");
+                printf("\n  |  NOME: ______________________________       CPF: _____________           |");
+                printf("\n  |                                                                          |");
+                printf("\n  |  TELEFONE: _______________       NASCIMENTO: __ / __ / ____              |");
+                printf("\n  |                                                                          |");
+                printf("\n  |  RUA: _____________________      N: ____    BAIRRO: ________________     |");
+                printf("\n  |                                                                          |");
+                printf("\n  |  CIDADE: _______________         ESTADO: __     CEP: _________           |");
+                printf("\n  |                                                                          |");
+                printf("\n  |  E-MAIL: ______________________________   DIAGNOSTICO: __ / __ / ____    |");
+                printf("\n  |                                                                          |");
+                printf("\n  |  COMORBIDADES: __________________________________________________        |");
+                printf("\n  |                                                                          |");
+                printf("\n  #--------------------------------------------------------------------------#");
+
+                SetCursorPosition(11, 5);
+                scanf("%s", &novoPaciente.nome);
+
+                SetCursorPosition(28, 5);
+                scanf("%s", &novoPaciente.cpf);
+
+                SetCursorPosition(28, 7);
+                scanf("%s", &novoPaciente.telefone);
+
+                SetCursorPosition(28, 7);
+                scanf("%i", &novoPaciente.nasc.dia);
+
+                SetCursorPosition(28, 7);
+                scanf("%i", &novoPaciente.nasc.mes);
+
+                SetCursorPosition(28, 7);
+                scanf("%i", &novoPaciente.nasc.ano);
+
+                SetCursorPosition(28, 9);
+                scanf("%s", &novoPaciente.endereco.rua);
+
+                SetCursorPosition(28, 9);
+                scanf("%s", &novoPaciente.endereco.n);
+
+                SetCursorPosition(28, 9);
+                scanf("%s", &novoPaciente.endereco.bairro);
+
+                SetCursorPosition(28, 11);
+                scanf("%s", &novoPaciente.endereco.cidade);
+
+                SetCursorPosition(28, 11);
+                scanf("%s", &novoPaciente.endereco.estado);
+
+                SetCursorPosition(28, 11);
+                scanf("%s", &novoPaciente.endereco.cep);
+
+                SetCursorPosition(28, 13);
+                scanf("%s", &novoPaciente.email);
+
+                SetCursorPosition(28, 13);
+                scanf("%i", &novoPaciente.diagnostico.dia);
+
+                SetCursorPosition(28, 13);
+                scanf("%i", &novoPaciente.diagnostico.mes);
+
+                SetCursorPosition(28, 13);
+                scanf("%i", &novoPaciente.diagnostico.ano);
+
+                SetCursorPosition(28, 15);
+                scanf("%s", &novoPaciente.comorbidades);
+
+                break;
+            }
 
             case 0:
-            break;
+            {
+                break;
+            }
 
             default:
+            {
                 printf("\n\n  [!] Opção inválida! Pressione ENTER para continuar...");
                 getch();
-            break;
+                break;
+            }
+
         }
 
     }
