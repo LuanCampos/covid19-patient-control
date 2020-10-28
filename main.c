2abd6e577a62973d87db328bf697e225c14f6f8a
Luan Campos <campos.luan@outlook.com>
2020-10-28 10:43:51 -0300

    Add login page

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ struct data
 typedef struct local LOCAL;
 struct local
 {
-    char rua[15];
+    char rua[20];
     char n[4];
     char bairro[15];
     char cidade[15];
@@ -35,7 +35,8 @@ struct paciente
     char comorbidades[50];
 };
 
-void SetCursorPosition(int XPos, int YPos) {
+void SetCursorPosition(int XPos, int YPos)
+{
    COORD coord;
    coord.X = XPos; coord.Y = YPos;
    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
@@ -45,32 +46,81 @@ int main()
 {
     setlocale(LC_ALL, "portuguese");
 
-    //Declarações
-    int opcao;
+    int opcao = 0;
+    int podeEntrar = 0;
 
-    //Instruções
+    // Página de Login
+    do
+    {
+        char loginCorreto[15] = "CadastroCovid";
+        char senhaCorreta[15] = "Doutor12345";
+        char login[15];
+        char senha[15];
+
+        system("cls");
+        printf("\n  #-------------------  CADASTRO DE PACIENTES (COVID 19)  -------------------#");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                               BEM-VINDO!                                 |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                  FAVOR INSERIR SEUS DADOS DE ACESSO:                     |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                  LOGIN:                                                  |");
+        printf("\n  |                  SENHA:                                                  |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  |                                                                          |");
+        printf("\n  #--------------------------------------------------------------------------#");
+
+        SetCursorPosition(28, 9);
+        scanf("%s", &login);
+
+        SetCursorPosition(28, 10);
+        scanf("%s", &senha);
+
+        SetCursorPosition(0, 20);
+
+        if (strcmp(login, loginCorreto) == 0 && strcmp(senha, senhaCorreta) == 0)
+        {
+            podeEntrar = 1;
+        }
+
+        else
+        {
+            printf("  [!] Dados incorretos! Pressione ENTER para continuar...");
+            podeEntrar = 0;
+            getch();
+        }
+
+    }
+    while (podeEntrar != 1);
+
+    // Menu Principal
     do
     {
         system("cls");
         printf("\n  #-------------------  CADASTRO DE PACIENTES (COVID 19)  -------------------#");
         printf("\n  |                                                                          |");
         printf("\n  |                                                                          |");
-        printf("\n  |                                MENU PRINCIPAL                            |");
+        printf("\n  |                             MENU PRINCIPAL                               |");
         printf("\n  |                                                                          |");
         printf("\n  |                                                                          |");
-        printf("\n  |          1 - CADASTRAR                                                   |");
-        printf("\n  |          2 - LISTAR                                                      |");
-        printf("\n  |          3 - REMOVER                                                     |");
-        printf("\n  |          0 - SAIR                                                        |");
+        printf("\n  |            1 - CONSULTAR LISTA DE PACIENTES                              |");
+        printf("\n  |            2 - CONSULTAR LISTA DE PACIENTES COM COMORBIDADES             |");
+        printf("\n  |            3 - CADASTRAR UM NOVO PACIENTE                                |");
+        printf("\n  |                                                                          |");
+        printf("\n  |            0 - SAIR                                                      |");
         printf("\n  |                                                                          |");
         printf("\n  |                                                                          |");
-        printf("\n  |     Programador:                                                         |");
-        printf("\n  |          Luan Campos                                                     |");
+        printf("\n  |                                                                          |");
         printf("\n  |                                                                          |");
         printf("\n  |                                                                          |");
         printf("\n  #--------------------------------------------------------------------------#");
         printf("\n\n  > Escolha uma opção: ");
-        SetCursorPosition(12, 12);
         scanf("%i", &opcao);
 
         switch(opcao)
@@ -78,23 +128,26 @@ int main()
             case 1:
                 //Instruções
             break;
+
             case 2:
                 //Instruções
             break;
+
             case 3:
                 //Instruções
             break;
+
             case 0:
-                //Finaliza
             break;
+
             default:
-                printf("\n\n [!] Opção inválida! Pressione ENTER para continuar...");
+                printf("\n\n  [!] Opção inválida! Pressione ENTER para continuar...");
                 getch();
             break;
         }
+
     }
     while (opcao != 0);
-
 
 
     return 0;
